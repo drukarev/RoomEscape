25c52f855c6ac6b177dec1c25c1a2c6ca513d749
drukarev <drukarev@yamoney.ru>
2019-01-09 01:46:06 +0300

    Add libgdx modules.

---
+++ desktop/src/com/room/game/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.room.game.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.room.game.RoomEscape;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new RoomEscape(), config);
+	}
+}
